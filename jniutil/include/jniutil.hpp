3706d94fde09ea4849ac6e10b27bdd8f07d3b089
Wiktor Stasiak <wstasiak@systherm-info.pl>
2015-03-24 14:08:11 +0100

    Added CPPJVM_LIB to allow use as a lib

--- jniutil/include/jniutil.hpp
+++ jniutil/include/jniutil.hpp
@@ -1,7 +1,9 @@
 #ifndef JVM_JNIUTIL_INCLUDED
 #define JVM_JNIUTIL_INCLUDED
 
-#if defined(CPPJVM_EXPORTS)
+#if defined(CPPJVM_LIB)
+#define CPPJVM_API 
+#elif defined(CPPJVM_EXPORTS)
 #define CPPJVM_API __declspec(dllexport)
 #else
 #define CPPJVM_API __declspec(dllimport)
